ace4b395fd61e03d5fe0b23f5b97a1189e7bf00d
kaeseolina <95242240+kaeseolina@users.noreply.github.com>
2021-11-29 21:16:15 +0300

    Add files via upload

---
+++ 2 .py
@@ -0,0 +1,14 @@
+import math
+
+a = 0
+float(a)
+a = 9.8
+
+Vi = 0
+float (Vi)
+
+d = float(input('Введите высоту в метрах: '))
+
+Vf = math.sqrt(Vi) + 2 * a * d
+
+print('Финальная скорость:',round(Vf,1),'м/с')
