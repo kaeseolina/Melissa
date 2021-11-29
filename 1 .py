5768f1ecf49361ea893e26b2e9c47bb7a2e4317d
kaeseolina <95242240+kaeseolina@users.noreply.github.com>
2021-11-29 22:09:22 +0300

    Update 1 .py

--- 1 .py
+++ 1 .py
@@ -1,6 +1,29 @@
-W = float(input('Введите ширину участка в футах: '))
+ start = time()
+        func(*args)
+        print(f'Время работы функции: {time() - start}')
+    return vremyafunc
 
-H = float(input('Введите длинну участка в футах: '))
+def decorat(func):
+    def name(*args):
+        print(f'Была вызвана функция {func.__name__}')
+        func(*args)
+    return name
 
-SQFT_PER_ACRE = (W * H)/43560
-print ('Вот площадь участка в акрах -',SQFT_PER_ACRE)
+
+@vremya
+@decorat
+def s_uchastka(l, w):
+    print(f'Площадь участка равна {round(l * w / 43560, 6)} акров')
+
+@vremya
+@decorat
+def svobodnoe_padenie(high):
+    vf = sqrt(0 ** 2) + 2 * 9.8 * high
+    print(f'скорость при приземлении равна {vf} м/c2')
+
+
+
+l, w = int(input('введите длину: ')), int(input('введите ширину: '))
+s_uchastka(l, w)
+h = int(input('сколько метров высота падения? '))
+svobodnoe_padenie(h)
