07c3d8e847eaa1521e7f76ba48086d1f6739a7a4
kaeseolina <95242240+kaeseolina@users.noreply.github.com>
2021-11-29 22:09:34 +0300

    Delete 2 .py

--- 2 .py
+++
@@ -1,8 +0,0 @@
-from math import sqrt
-
-def svobodnoe_padenie(high):
-    vf = sqrt(0 ** 2) + 2 * 9.8 * high
-    print(f'скорость при приземлении равна {vf} м/c2')
-
-h = int(input('сколько метров высота падения? '))
-svobodnoe_padenie(h)
