71daf22ddea584e07b33eac4b65b26838e814af9
kaeseolina <95242240+kaeseolina@users.noreply.github.com>
2021-11-29 22:23:29 +0300

    Update 1 .py

--- 1 .py
+++ 1 .py
@@ -1,11 +1,16 @@
- start = time()
+from math import sqrt
+from time import time
+
+def vremya(func):
+    def vremyafunc(*args):
+        start = time()
         func(*args)
-        print(f'Время работы функции: {time() - start}')
+        print(f'Время работы ф: {time() - start}')
     return vremyafunc
 
 def decorat(func):
     def name(*args):
-        print(f'Была вызвана функция {func.__name__}')
+        print(f'вызвана функция {func.__name__}')
         func(*args)
     return name
 
@@ -13,13 +18,14 @@ def decorat(func):
 @vremya
 @decorat
 def s_uchastka(l, w):
-    print(f'Площадь участка равна {round(l * w / 43560, 6)} акров')
+    print(f'Площадь равна {round(l * w / 43560, 6)} акров')
 
 @vremya
 @decorat
 def svobodnoe_padenie(high):
     vf = sqrt(0 ** 2) + 2 * 9.8 * high
-print(f'скорость при приземлении равна {vf} м/c')
+    print(f'скорость при приземлении равна {vf} м/c')
+
 
 
 l, w = int(input('введите длину: ')), int(input('введите ширину: '))
