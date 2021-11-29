aab4c42e618b724f0459368edd47ca48f01a3142
kaeseolina <95242240+kaeseolina@users.noreply.github.com>
2021-11-29 21:44:01 +0300

    Update 2 .py

--- 2 .py
+++ 2 .py
@@ -8,7 +8,5 @@ Vi = 0
 float (Vi)
 
 d = float(input('Введите высоту в метрах: '))
-
-Vf = math.sqrt(Vi) + 2 * a * d
-
+Vf = math.sqrt(Vi**2 + 2 * a *d)
 print('Финальная скорость:',round(Vf,1),'м/с')
